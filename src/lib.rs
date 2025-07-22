634404b10aee99b60bcf43ac00cc149226b0b642
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-07-22 14:34:34 -0700

    Implement fetching next in index

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use std::hash::Hash;
 use std::ops::Deref;
 use std::sync::{Arc, RwLock};
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::mpsc::SendError;
 use std::thread;
@@ -16,8 +16,8 @@ pub struct Extractdb<V: Send + Sync + Eq + Hash> {
     accessible_index: AtomicUsize
 }
 
-impl<V: Send + Sync + Eq + Hash + 'static> Extractdb<V> {
-    pub fn new<K: Send + Sync + Eq + Hash + 'static>() -> Extractdb<V> {
+impl<V: Send + Sync + Eq + Hash + Clone + 'static> Extractdb<V> {
+    pub fn new<K: Send + Sync + Eq + Hash + Clone + 'static>() -> Extractdb<V> {
         let (tx, rx) = channel();
 
         let data_store = Arc::new(RwLock::new(HashSet::new()));
@@ -58,7 +58,43 @@ impl<V: Send + Sync + Eq + Hash + 'static> Extractdb<V> {
         self.data_sender.send(item)
     }
 
-    pub fn internal_count(&self) -> Result<usize, Box<dyn Error>> {
+    pub fn fetch_next(&mut self) -> Result<V, Box<dyn Error + '_>> {
+        let accessible_index = self.accessible_index.load(Ordering::Relaxed);
+        let accessible_store_len = self.count()?;
+
+        // TODO: While this is a basic "algorithm" it could be improved...
+        // The current implementation works by loading more data into the store whenever the index reaches the current length.
+        // The problem with this only occurs later on in large sets when the accessible_store index is small and the data_store is large.
+        // After reaching the end it loads a massive amount of memory (e.g. the entire data_store) causing deadlocks and slowdowns.
+        if accessible_index >= accessible_store_len {
+            let data_store_reader = self.data_store.read()?;
+            let mut accessible_store_writer =  self.accessible_store.write()?;
+
+            // TODO: There might a better way to handle data loading from data_store to accessible_store
+            *accessible_store_writer = Vec::from_iter(data_store_reader.deref().clone());
+        }
+
+        self.accessible_index.fetch_add(1, Ordering::Relaxed);
+        if let Ok(accessible_store_reader) = self.accessible_store.read() {
+            let value = accessible_store_reader.get(accessible_index);
+
+            return match value {
+                Some(value) => Ok(value.clone()),
+                None => Err("No new data could be retrieved from temporary accessible_store".into())
+            }
+        }
+
+        Err("Failed to access data from leaky accessible_store".into())
+    }
+
+    pub fn count(&self) -> Result<usize, Box<dyn Error>> {
+        self.accessible_store
+            .read()
+            .map(|reader| reader.len())
+            .map_err(|err| format!("Failed to retrieve internal count of data_store ({})", err).into())
+    }
+
+    pub fn internal_count(&self) -> Result<usize, Box<dyn Error + Send + Sync>> {
         if let Ok(locked_store) = self.data_store.read() {
             return Ok(locked_store.len());
         }
